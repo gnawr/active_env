fixed feature scaling

src/learners/demo_learner.py
@@ -17,8 +17,8 @@ class DemoLearner(object):
 		self.num_features = len(self.feat_list)
 		self.environment = environment
 
-		FEAT_RANGE = constants["FEAT_RANGE"]
-		self.feat_range = [FEAT_RANGE[self.feat_list[feat]] for feat in range(self.num_features)]
+		# FEAT_RANGE = constants["FEAT_RANGE"]
+		# self.feat_range = [FEAT_RANGE[self.feat_list[feat]] for feat in range(self.num_features)]
 
 		# Set up discretization of theta and beta space.
 		self.betas_list = constants["betas_list"]
@@ -45,9 +45,20 @@ class DemoLearner(object):
 		for rand_i, traj_str in enumerate(self.traj_rand.keys()):
 			curr_traj = np.array(ast.literal_eval(traj_str))
 			rand_features = self.environment.featurize(curr_traj, self.feat_list)
-			Phi_rand = np.array([sum(x)/self.feat_range[i] for i,x in enumerate(rand_features)])
+			# Phi_rand = np.array([sum(x)/self.feat_range[i] for i,x in enumerate(rand_features)])
+
+			Phi_rand = np.array([sum(x) for i,x in enumerate(rand_features)])
 			self.Phi_rands.append(Phi_rand)
 
+		self.Phi_rands = np.array(self.Phi_rands)
+
+		# Create scaling coeffs
+		self.max_features = np.max(self.Phi_rands, axis=0)
+		self.min_features = np.min(self.Phi_rands, axis=0)
+		self.scaling_coeffs = [{"min": self.min_features[i], "max": self.max_features[i]} for i in np.arange(self.num_features)]
+
+		# Todo apply scaling coeffs
+		self.Phi_rands = (self.Phi_rands - self.min_features) / (self.max_features - self.min_features)
 
 	def calc_obs_model(self, trajs):
 		if isinstance(trajs[0], np.ndarray):
@@ -55,7 +66,10 @@ class DemoLearner(object):
 		else: 
 			new_features = [np.sum(self.environment.featurize(traj.waypts, self.feat_list), axis=1) for traj in trajs]
 			
-		Phi_H = np.array(np.sum(np.matrix(new_features), axis=0) / self.feat_range).T
+		# Apply scaling coefficients
+		summed = np.array(np.sum(np.matrix(new_features), axis=0))
+		Phi_H = (summed - self.min_features) / (self.max_features - self.min_features)
+		Phi_H = Phi_H.T
 		print "Phi_H: ", Phi_H
 
 		# Now compute probabilities for each beta and theta pair.

src/test.py
@@ -217,7 +217,42 @@ class DevTest(unittest.TestCase):
 		print 'FEAT MAX: ', np.max(features, axis=0)
 		print 'FEAT MIN: ', np.min(features, axis=0)
 
-		
-if __name__ == '__main__':
-	unittest.main()
+	def test_obs_model(self):
+		model_filename = 'jaco_dynamics'
+		human_center = [-0.6,0.55,0.0]
+		laptop_center = [-0.7929,-0.1,0.0]
+		object_centers = {'HUMAN_CENTER': human_center, 'LAPTOP_CENTER': laptop_center}
+		feat_list = ["efficiency", "human", "laptop"]
+		constants = {'trajs_path': "/data/traj_sets/traj_rand_merged_H.p",
+					 'betas_list': [10.0],
+					 'weight_vals': [0.0, 0.5, 1.0], # Per feature theta options.
+					 'FEAT_RANGE': {'table':0.98, 'coffee':1.0, 'laptop':0.4, 'human':0.4, 'efficiency':0.22},
+					 }
+		FEAT_RANGE = {'table':0.98, 'coffee':1.0, 'laptop':0.4, 'human':0.4, 'efficiency':0.22}
 
+		
+		env = Environment(model_filename, object_centers)
+		learner = DemoLearner(feat_list, env, constants)
+		true_weight = learner.weights_list[-1] # 1, 0, 1
+
+
+		features = np.array(learner.Phi_rands)
+		self.assertTrue((np.max(features, axis=0) <= 1).all())
+		self.assertTrue((np.min(features, axis=0) >= 0).all())
+
+		# Get a demo to test obs model
+		max_iter = 50
+		num_waypts = 5
+		start = np.array([104.2, 151.6, 183.8, 101.8, 224.2, 216.9, 310.8]) * math.pi / 180.0
+		goal = np.array([210.8, 101.6, 192.0, 114.7, 222.2, 246.1, 322.0]) * math.pi / 180.0
+		goal_pose = None
+		T = 20.0
+		timestep = 0.5 
+
+		planner = TrajoptPlanner(feat_list, max_iter, num_waypts, env)
+		planned_traj = [planner.replan(start, goal, goal_pose, true_weight, T, timestep)]
+		P_obs = learner.calc_obs_model(planned_traj)
+
+if __name__ == '__main__':
+    unittest.main()
+		